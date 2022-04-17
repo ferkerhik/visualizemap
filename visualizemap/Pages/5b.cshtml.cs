04e5f4608a96cac313fe122f3383f6d617206687
Pangpond220 <62050220@kmitl.ac.th>
2022-04-17 19:13:42 +0700

    query 5b

--- visualizemap/Pages/5b.cshtml.cs
+++ visualizemap/Pages/5b.cshtml.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +13,8 @@ namespace visualizemap.Pages
 {
     public class _5bModel : PageModel
     {
+        public string jsontext;
+        //public int year;
         private readonly ILogger<_5bModel> _logger;
 
         public _5bModel(ILogger<_5bModel> logger)
@@ -19,9 +24,33 @@ namespace visualizemap.Pages
 
         public void OnGet()
         {
+            database();
+        }
 
+        public void OnPost()
+        {
+
+            //year = Int32.Parse(Request.Form["year"]);
+            database();
+        }
+        public void database()
+        {
+            string CS = "Data Source=LAPTOP-H78D00F6\\MSSQLSERVER144;Initial Catalog=SpatialDB3;Integrated Security=True";   //DB Chan
+            //string CS = "Data Source=LAPTOP-QG030MKA;Initial Catalog=SpatialDB3;Integrated Security=True";            //DB Pond
+            SqlConnection con = new SqlConnection(CS);
+            string command = "declare @bangkok geometry select @bangkok = Geom from AirPollutionPM25 where city = 'Bangkok' select distinct top 50 city, country, latitude, longitude, Geom.MakeValid().STDistance(@bangkok) dist from AirPollutionPM25 where city <> 'Bangkok' order by dist asc";
+
+            SqlDataAdapter da = new SqlDataAdapter(command, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            string[] columnNames = (from dc in dt.Columns.Cast<DataColumn>()
+                                    select dc.ColumnName).ToArray();
+            Console.WriteLine(dt.DataSet);
+
+            Console.WriteLine("Test2");
+
+            jsontext = JsonConvert.SerializeObject(dt);
+            Console.WriteLine(jsontext);
         }
     }
-
-
 }
