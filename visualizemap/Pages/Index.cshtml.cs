81543394dbcd904458db193a0d445558eb7d654c
ferkerhik <73007558+ferkerhik@users.noreply.github.com>
2022-04-17 13:20:53 +0700

    Add project files.

---
+++ visualizemap/Pages/Index.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace visualizemap.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string jsontext;
+        public int year;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            database();
+        }
+
+        public void OnPost() {
+            
+            year = Int32.Parse(Request.Form["year"]);
+            database();
+        }
+        public void database()
+        {
+            string CS = "Data Source=LAPTOP-H78D00F6\\MSSQLSERVER144;Initial Catalog=SpatialDB3;Integrated Security=True";
+            SqlConnection con = new SqlConnection(CS);
+            string command = "select latitude,longitude from AirPolluTionPM25 where year = '"+year+"'";
+
+            SqlDataAdapter da = new SqlDataAdapter(command, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            string[] columnNames = (from dc in dt.Columns.Cast<DataColumn>()
+                                    select dc.ColumnName).ToArray();
+            Console.WriteLine(dt.DataSet);
+
+            Console.WriteLine("Test2");
+
+            jsontext = JsonConvert.SerializeObject(dt);
+            Console.WriteLine(jsontext);
+        }
+    }
+
+}
+
+
