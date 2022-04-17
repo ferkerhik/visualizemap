2e27ed5aefb99602cb497ee2883fda06dee58ef9
Pangpond220 <62050220@kmitl.ac.th>
2022-04-17 19:53:46 +0700

    onlt query

--- visualizemap/Pages/5a.cshtml.cs
+++ visualizemap/Pages/5a.cshtml.cs
@@ -35,8 +35,8 @@ namespace visualizemap.Pages
         }
         public void database()
         {
-            string CS = "Data Source=LAPTOP-H78D00F6\\MSSQLSERVER144;Initial Catalog=SpatialDB3;Integrated Security=True";   //DB Chan
-            //string CS = "Data Source=LAPTOP-QG030MKA;Initial Catalog=SpatialDB3;Integrated Security=True";            //DB Pond
+            //string CS = "Data Source=LAPTOP-H78D00F6\\MSSQLSERVER144;Initial Catalog=SpatialDB3;Integrated Security=True";   //DB Chan
+            string CS = "Data Source=LAPTOP-QG030MKA;Initial Catalog=SpatialDB3;Integrated Security=True";            //DB Pond
             SqlConnection con = new SqlConnection(CS);
             string command = "select latitude,longitude,color_pm25 from AirPolluTionPM25 where year = '" + year + "'";//5(a)
             //string command = "select latitude,longitude,color_pm25 from AirPolluTionPM25";
--- visualizemap/Pages/5f.cshtml.cs
+++ visualizemap/Pages/5f.cshtml.cs
@@ -1,16 +1,56 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace visualizemap.Pages
 {
     public class _5fModel : PageModel
     {
+        public string jsontext;
+        //public int year;
+        private readonly ILogger<_5fModel> _logger;
+
+        public _5fModel(ILogger<_5fModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
+            database();
+        }
+
+        public void OnPost()
+        {
+
+            //year = Int32.Parse(Request.Form["year"]);
+            database();
+        }
+        public void database()
+        {
+            //string CS = "Data Source=LAPTOP-H78D00F6\\MSSQLSERVER144;Initial Catalog=SpatialDB3;Integrated Security=True";   //DB Chan
+            string CS = "Data Source=LAPTOP-QG030MKA;Initial Catalog=SpatialDB3;Integrated Security=True";            //DB Pond
+            SqlConnection con = new SqlConnection(CS);
+            string command = "declare @bk geometry select @bk = Geom from AirPollutionPM25 where city = 'Bangkok' select distinct top 50 city, country, latitude, longitude, Geom.MakeValid().STDistance(@bk) dist from AirPollutionPM25 where city != 'Bangkok' order by dist asc";
+
+            SqlDataAdapter da = new SqlDataAdapter(command, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            string[] columnNames = (from dc in dt.Columns.Cast<DataColumn>()
+                                    select dc.ColumnName).ToArray();
+            Console.WriteLine(dt.DataSet);
+
+            Console.WriteLine("Test2");
+
+            jsontext = JsonConvert.SerializeObject(dt);
+            Console.WriteLine(jsontext);
         }
     }
 }
