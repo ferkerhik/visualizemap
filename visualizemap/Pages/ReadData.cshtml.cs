5b913226c6e3b0554b6dec650cfb1a4605b2e873
Pangpond220 <62050220@kmitl.ac.th>
2022-04-18 15:28:19 +0700

    add Readdata

---
+++ visualizemap/Pages/ReadData.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Configuration;
+using ClosedXML.Excel;
+using System.IO;
+
+namespace visualizemap.Pages
+{
+    public class ReadDataModel : PageModel
+    {
+        
+    }
+}
+
+
